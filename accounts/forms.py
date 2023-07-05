13d1842e5585c8eb270a18162885a0a75e6794b0
hosseinasghari <ho3ein.m1991@gmail.com>
2023-07-05 13:34:41 +0330

    fix forms accounts and edit_profile

--- accounts/forms.py
+++ accounts/forms.py
@@ -27,10 +27,53 @@ class UserLoginForm(forms.Form):
 
 
 class UserEditProfileForm(forms.ModelForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={"placeholder": "username"}
+        )
+    )
+    firstname = forms.CharField(
+        widget=forms.TextInput(
+            attrs={"placeholder": "firstname"}
+            )
+    )
+    lastname = forms.CharField(
+        widget=forms.TextInput(
+            attrs={"placeholder": "lastname"}
+            )
+    )
+    bio = forms.CharField(
+        required=False,
+        widget=forms.TextInput(
+            attrs={"placeholder": "bio"},
+            )
+    )
+    gender_choices = (
+        (1, 'Male'),
+        (2, 'Female'),
+    )
+    gender = forms.ChoiceField(
+        choices=gender_choices,
+        widget=forms.Select(
+            attrs={"placeholder": "gender"}
+            )
+    )
+    phonenumber = forms.CharField(
+        widget=forms.TextInput(
+            attrs={"placeholder": "phonenumber"}
+            )
+    )
+    date_of_birth = forms.DateField(
+        required=False,
+        widget=forms.TextInput(
+            attrs={"placeholder": "date_of_birth"}
+            )
+    )
+
     class Meta:
         model = MyUser
         fields = [
-            'firstname', 'lastname',
+            'username', 'firstname', 'lastname',
             'bio', 'gender',
             'phonenumber', 'date_of_birth'
             ]
